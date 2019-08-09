2ad5b458ba9128956428b43dd4ffa9cc9a347aa7
Diana Badas <diana.badas@gmail.com>
2019-08-09 12:29:35 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,6 +15,16 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def description
+    @product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    @product = Product.find(params[:id])
+    render plain: product.inventory > 0? "true" : "false"
+  end
+
   private
 
   def product_params
