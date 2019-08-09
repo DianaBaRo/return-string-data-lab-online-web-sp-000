85b4121da97f1c22b6bcf74733b059f1b6d3bfeb
Diana Badas <diana.badas@gmail.com>
2019-08-09 12:20:02 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,24 @@
 class ProductsController < ApplicationController
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.build(product_params)
+    @product.save
+
+    redirect_to product_path(@product)
+  end
+
   def index
     @products = Product.all
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :inventory)
   end
 
 end
